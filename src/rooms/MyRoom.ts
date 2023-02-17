fce3813452856c0f57a81c1d7f86fef0421b4f83
perplexedyawdie <sunkwenga@gmail.com>
2023-02-17 12:30:13 -0500

    added extra room logic

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -6,16 +6,44 @@ export class MyRoom extends Room<MyRoomState> {
   onCreate (options: any) {
     this.setState(new MyRoomState());
 
-    this.onMessage("type", (client, message) => {
+    this.onMessage("PLAYER_MOVED", (client, message) => {
       //
       // handle "type" message
       //
+      console.log("player moved")
+      // console.log(client)
+      console.log(message)
+      this.broadcast("PLAYER_MOVED", message, { except: client });
+
+    });
+
+    this.onMessage("YOUR_TURN", (client, message) => {
+      //
+      // handle "type" message
+      //
+      console.log("turn changed")
+      // console.log(client)
+      console.log(message)
+      this.broadcast("YOUR_TURN", message, { except: client });
+
+    });
+
+    this.onMessage("GAME_END", (client, message) => {
+      //
+      // handle "type" message
+      //
+      console.log("GAME_END")
+      // console.log(client)
+      console.log(message)
+      this.broadcast("GAME_END", true);
+
     });
 
   }
 
   onJoin (client: Client, options: any) {
     console.log(client.sessionId, "joined!");
+    this.broadcast("PLAYER_JOINED", true, { except: client });
   }
 
   onLeave (client: Client, consented: boolean) {
